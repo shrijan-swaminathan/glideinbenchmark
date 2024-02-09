f04645ae0a00f758fdbf7bad5767b59821c46592
swamina7 <shrijanswaminathan@gmail.com>
2024-02-09 08:55:17 -0600

    works with both directories and file inputs

--- src/glideinbenchmark/runner_config.py
+++ src/glideinbenchmark/runner_config.py
@@ -10,25 +10,31 @@ import os
 import subprocess
 
 from flask import Flask, render_template, request
-
 from glideinbenchmark import factory_parser
 
+# RUN THIS BEFORE RUNNING THE FILE export PYTHONPATH="$PYTHONPATH:/home/swamina7/GlideinBenchmark_webapp/glideinbenchmark/src/"
+
 app = Flask(__name__)
-xml_file_dir = "/etc/gwms-factory/glideinWMS.xml"  # './xml_files' # should be /etc/gwms-factory/config.d but is xml_files temporarily
-# xml_file = 'temp_factory.xml'
+# xml_file_dir = "/etc/gwms-factory/glideinWMS.xml"  # './xml_files' # should be /etc/gwms-factory/config.d but is xml_files temporarily
+# xml_file_dir = './temp_factory.xml'
+# xml_file_dir = 'temp_factory.xml'
+# xml_file_dir = './testing.xml'
+# xml_file_dir = './'
 script_path = (
     factory_parser.__file__
-)  # './factory_parser.py' #will update this for absolute path later
+)
 
+app.config['XML_CONFIG'] = xml_file_dir
 
 @app.route("/", methods=["GET", "POST"])
-def index(xml_config):
-    """Creates the page controlling the Factory configuration
-
-    Params:
-        xml_config(str): path of the Factory xml configuration file, or a folder containing Factory configuration files.
-                         if it is a file, also the config.d subdirectory of its directory is controlled for configuration files
+def index():
     """
+    Creates the page controlling the Factory configuration
+    This function handles the index route of the Flask application.
+    The xml_config path is set in the Flask application's configuration before the application is run.
+    """
+    xml_config = app.config['XML_CONFIG']
+    print(xml_config.endswith(".xml") and os.path.isfile(xml_config))
     if xml_config.endswith(".xml") and os.path.isfile(xml_config):
         xml_files = [xml_config]
         xml_config = os.path.join(os.path.dirname(xml_config), "config.d")
@@ -37,6 +43,7 @@ def index(xml_config):
     # get a list of all the xml files in the directory
     if os.path.isdir(xml_config):
         xml_files += [f for f in os.listdir(xml_config) if f.endswith(".xml")]
+    print(xml_files)
     # set a dict to store all the entries in
     entries = {}
     # Check if there are any xml files in the directory
@@ -45,10 +52,10 @@ def index(xml_config):
         return render_template("index.html", entries=[])
     # go through all the xml files and get the entries in each
     for xml_file in xml_files:
-        xml_name = xml_file.split(".")[0]
-        xml_file = os.path.join(xml_file_dir, xml_file)
+        xml_name = os.path.basename(xml_file).split(".")[0]
+        xml_file = os.path.basename(xml_file)
         # check if file exists and if not, return empty list
-        if factory_parser.check_file_exists(xml_file) == 0:
+        if factory_parser.check_file_exists(xml_file) == False:
             print("factory location incorrect")
             return render_template("index.html", entries=[])
         if request.method == "POST":
@@ -102,4 +109,3 @@ def index(xml_config):
 
 if __name__ == "__main__":
     app.run(debug=True)
-    index(xml_file_dir)
